b45a6f238f02e09da272074e4fe31e7f38c6c7c3
CirnoV <cirnov59@gmail.com>
2021-04-28 00:05:19 +0900

    prettier

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-import {Command, flags} from "@oclif/command";
-import fs from "fs";
-import OsuParser from "osu-json-parser";
-import path from "path";
-import readline from "readline";
-import parseRGB from "./parseRGB";
+import { Command, flags } from '@oclif/command';
+import fs from 'fs';
+import OsuParser from 'osu-json-parser';
+import path from 'path';
+import readline from 'readline';
+import parseRGB from './parseRGB';
 
-async function start(file: string, name: string, musicExt: string, offset: number) {
+async function start(
+  file: string,
+  name: string,
+  musicExt: string,
+  offset: number
+): Promise<void> {
   let filepath = `${path.basename(name)}.inc`;
   filepath = path.join(path.dirname(file), filepath);
   const rl = readline.createInterface({
@@ -15,12 +20,17 @@ async function start(file: string, name: string, musicExt: string, offset: numbe
   const parser = new OsuParser();
 
   return new Promise((resolve) => {
-    rl.on("line", (line) => {
+    rl.on('line', (line) => {
       parser.parse(line);
     });
 
-    rl.on("close", () => {
-      const scripts = parseRGB(name, parser.toJSON(), musicExt.replace(".", ""), offset);
+    rl.on('close', () => {
+      const scripts = parseRGB(
+        name,
+        parser.toJSON(),
+        musicExt.replace('.', ''),
+        offset
+      );
       fs.writeFileSync(filepath, scripts);
       // const scripts = JSON.stringify(parser.toJSON(), null, 2);
       // fs.writeFileSync(filepath, scripts);
@@ -30,22 +40,31 @@ async function start(file: string, name: string, musicExt: string, offset: numbe
 }
 
 class RGBOsu extends Command {
-  public static description = "OSU! 채보를 RGB 채보로 변환합니다.";
+  public static description = 'OSU! 채보를 RGB 채보로 변환합니다.';
 
   public static flags = {
-    name: flags.string({char: "n", required: true, description: "음원 이름 (example.mp3)"}),
-    offset: flags.string({char: "o", description: "offset (milliseconds)"}),
+    name: flags.string({
+      char: 'n',
+      required: true,
+      description: '음원 이름 (example.mp3)',
+    }),
+    offset: flags.string({ char: 'o', description: 'offset (milliseconds)' }),
   };
 
-  public static args = [{name: "file"}];
+  public static args = [{ name: 'file' }];
 
   public async run() {
-    const {args, flags} = this.parse(RGBOsu);
+    const { args, flags } = this.parse(RGBOsu);
 
     if (args.file) {
       const offset = Number(flags.offset);
-      const name = flags.name.replace(/[^\x00-\x7F]/g, "");
-      start(args.file, path.basename(name, path.extname(name)), path.extname(name), Number.isNaN(offset) ? 0 : offset);
+      const name = flags.name.replace(/[^\x00-\x7F]/g, '');
+      start(
+        args.file,
+        path.basename(name, path.extname(name)),
+        path.extname(name),
+        Number.isNaN(offset) ? 0 : offset
+      );
     }
   }
 }
